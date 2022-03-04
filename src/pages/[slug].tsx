9a8997f26aef49860f38f34f20a4e24d0495f452
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-03-05 01:15:10 +0700

    feat: add post content page

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,26 @@
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+
+import Head from 'components/head'
+
+const Index = () => {
+  const router = useRouter()
+  const { slug } = router.query
+  const { data, error } = useSWR(`https://fswd-wp.devnss.com/wp-json/wp/v2/posts?slug=${slug}`)
+
+  return (
+    <>
+      <Head title='Home' />
+      <div className='max-w-screen-md mx-auto py-3 flex flex-row flex-wrap'>
+        {data && !error && (
+          <>
+            <div className='mb-2 text-2xl font-medium'>{data[0]?.title.rendered}</div>
+            <div dangerouslySetInnerHTML={{ __html: data[0]?.content.rendered }}></div>
+          </>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Index
