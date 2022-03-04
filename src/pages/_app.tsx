480c46c1f209d55141357d051688996961b03e12
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-03-05 01:12:25 +0700

    feat: add global swr config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import type { AppProps } from 'next/app'
+import ky from 'ky'
+import { SWRConfig } from 'swr'
+
 import NavBar from 'components/navbar'
 
 import 'styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <SWRConfig
+      value={{
+        fetcher: (url) => ky(url).json(),
+      }}
+    >
       <NavBar />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   )
 }
 
