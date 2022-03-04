9ef22f85418bfdd9a7171e93998983d21073a6d2
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-03-05 01:13:38 +0700

    fix: fix navbar style

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,15 +7,15 @@ const NavBar: React.FC = () => {
   const { asPath: pagePath } = useRouter()
 
   return (
-    <div className='mb-14 z-50'>
-      <div className='w-full top-0 fixed bg-white border-b border-gray-200'>
-        <div className='max-w-screen-xl h-14 flex flex-row items-center justify-between mx-auto p-2'>
+    <div className='mb-14'>
+      <div className='w-full top-0 z-50 fixed bg-white border-b border-gray-200'>
+        <div className='max-w-screen-lg h-14 flex flex-row items-center justify-between mx-auto p-2'>
           <h1 className='text-4xl'>
             <span className='font-extralight'>blog</span>null
           </h1>
           <div className='flex justify-end'>
             {navMenuItems.map(({ title, path }) => (
-              <Link href={path} key={title.toLocaleLowerCase()}>
+              <Link href={path} key={title.toLowerCase()}>
                 <a
                   className={`${
                     pagePath === path
