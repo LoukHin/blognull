a1aee7933105fef18f3388372ed4f853692e8f34
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-03-05 01:15:01 +0700

    feat: add post list to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
+import useSWR from 'swr'
+
 import Head from 'components/head'
+import PostImage from 'components/image'
+import Link from 'next/link'
 
 const Index = () => {
+  const { data, error } = useSWR('https://fswd-wp.devnss.com/wp-json/wp/v2/posts')
+
   return (
     <>
       <Head title='Home' />
-      Hello world
+      <div className='max-w-screen-md mx-auto py-3 flex flex-row flex-wrap'>
+        {data?.map((post) => (
+          <Link key={`${post.id}`} href={`${post.slug}`}>
+            <a className='w-full mx-auto my-2'>
+              <div className='rounded overflow-hidden ring-1 ring-gray-200 duration-150 hover:shadow'>
+                <PostImage featuredMedia={post.featured_media} alt={post.title.rendered} />
+                <div className='p-3'>
+                  <div className='mb-2 text-2xl font-medium'>{post.title.rendered}</div>
+                  <div dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></div>
+                </div>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </div>
     </>
   )
 }
