3df8d8eb67cc43f66ada3bf8f9d87d8fbe444dce
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-03-05 19:49:18 +0700

    fix: remove x padding

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -11,21 +11,23 @@ const Category = () => {
   return (
     <>
       <Head title='Category' />
-      <div className='max-w-screen-md mx-auto py-3 flex flex-row flex-wrap'>
+      <div className='max-w-screen-md mx-auto py-3'>
         <span className='w-full text-2xl'>Category</span>
         <hr className='w-full my-3 border-b border-gray-200' />
-        {categories?.map((category) => (
-          <div key={`${category.id}`} className='w-1/3 p-1'>
-            <Link href={`/category/${category.slug}`}>
-              <a className='w-full'>
-                <div className='p-3 rounded overflow-hidden ring-1 ring-gray-200 duration-150 hover:shadow-lg hover:ring-gray-400'>
-                  <div className='text-2xl font-medium'>{category.name}</div>
-                  {category.description && <div className='mt-2'>{category.description}</div>}
-                </div>
-              </a>
-            </Link>
-          </div>
-        ))}
+        <div className='flex flex-row flex-wrap -mx-2'>
+          {categories?.map((category) => (
+            <div key={`${category.id}`} className='w-1/3 p-2'>
+              <Link href={`/category/${category.slug}`}>
+                <a className='w-full'>
+                  <div className='p-3 rounded overflow-hidden ring-1 ring-gray-200 duration-150 hover:shadow-lg hover:ring-gray-400'>
+                    <div className='text-2xl font-medium'>{category.name}</div>
+                    {category.description && <div className='mt-2'>{category.description}</div>}
+                  </div>
+                </a>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
