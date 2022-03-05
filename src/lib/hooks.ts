bdefa2be12aa94f1b355b868d3b0ebeb5604062a
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-03-05 17:37:37 +0700

    perf: sent request only with valid dependencies

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,23 +1,30 @@
 import ky from 'ky'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import config from 'lib/config'
 
-const useWordpressApi = (path: string) => {
-  const [data, setData] = useState<any>()
+const useWordpressApi = <T = any>(path: string, dependencies: any[] = []) => {
+  const [data, setData] = useState<T>()
   const [error, setError] = useState()
 
+  const canSentRequest = useMemo(
+    () => !dependencies.some((dependency) => !dependency),
+    [dependencies]
+  )
+
   useEffect(() => {
-    ky.get(path, {
-      prefixUrl: config.cmsApiUrl,
-      headers: {
-        Authorization: config.cmsApiAuthorization,
-      },
-    })
-      .json()
-      .then(setData)
-      .catch(setError)
-  }, [path])
+    if (canSentRequest) {
+      ky.get(path, {
+        prefixUrl: config.cmsApiUrl,
+        headers: {
+          Authorization: config.cmsApiAuthorization,
+        },
+      })
+        .json<T>()
+        .then(setData)
+        .catch(setError)
+    }
+  }, [path, canSentRequest])
 
   return { data, error }
 }
