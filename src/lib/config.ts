3bd4669d7a2e1bf0fd53eaa46685751970d7d975
Supakit Khawmeewong <loukhin@skepticalsaurus.land>
2022-03-05 13:34:47 +0700

    feat: read cms info from env

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,7 @@
 const config = {
   NODE_ENV: process.env.NODE_ENV,
+  cmsApiUrl: process.env.NEXT_PUBLIC_CMS_API_URL,
+  cmsApiAuthorization: process.env.NEXT_PUBLIC_CMS_API_AUTHORIZATION,
   appName: process.env.NEXT_PUBLIC_APP_NAME,
   appUrl: process.env.NEXT_PUBLIC_APP_URL,
 }
